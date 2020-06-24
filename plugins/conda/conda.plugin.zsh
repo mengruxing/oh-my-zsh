d778de4ce3d509df4aa6d54cd9c322eb5802b0e3
mengruxing <mengruxing@gmail.com>
2020-06-24 12:29:02 +0800

    add conda to plugins

---
+++ plugins/conda/conda.plugin.zsh
@@ -0,0 +1,11 @@
+function activate-conda-venv() {
+    LAST_PS1=$PS1
+    source activate "$@"
+    if [ $? -eq 0 ]; then
+        VIRTUAL_ENV="$@"
+        PS1=$LAST_PS1
+    fi
+    unset LAST_PS1
+}
+
+alias acvenv='activate-conda-venv'
